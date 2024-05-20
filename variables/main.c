0cbc3c7fe9af91107ce9222155d1d788b7f063a0
Danvaspri <andresvp2002@gmail.com>
2024-05-20 12:14:45 -0500

    variables

---
+++ variables/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+main(){
+  int integer_var = 5;
+  float float_var = 3.140000;
+  char char_var[13]= "Hello, World!";
+  printf("%d is an integer\n",integer_var);
+  printf("%f is a float\n",float_var);
+  printf("%s is a char\n",char_var);
+}
